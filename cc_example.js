e0c1d6c4f1227d890e03d414221e4e1c5d24a9f0
Ashley Le <ashleyle@usf.edu>
2025-02-11 16:06:18 -0500

    completed task 1

---
+++ cc_example.js
@@ -0,0 +1,13 @@
+// Task 1
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"}, //T
+    {name: "Notebook", price: 5, category: "Stationery"} //F
+];
+
+// function getProductsByCategory(products, category) {
+//     return products.filter(product => product.category === category);
+// }
+
+// console.log(getProductsByCategory(products, "Electronics"));
+
+// Task 2
