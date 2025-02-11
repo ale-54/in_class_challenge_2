b405d0c77390ec2cf73e0ff97643f6dfbf2300b7
Ashley Le <ashleyle@usf.edu>
2025-02-11 16:39:19 -0500

    full commit pt2 cuz I am the idiot and kept hte code as //

--- cc_example.js
+++ cc_example.js
@@ -4,33 +4,33 @@ let products = [
     {name: "Notebook", price: 5, category: "Stationery"} //F
 ];
 
-// function getProductsByCategory(products, category) {
-//     return products.filter(product => product.category === category);
-// }
+function getProductsByCategory(products, category) {
+    return products.filter(product => product.category === category);
+}
 
-// console.log(getProductsByCategory(products, "Electronics"));
+console.log(getProductsByCategory(products, "Electronics"));
 
 // Task 2
-//  function applyDiscount(products, discountRate) {
-//      return products.map(product => ({
-//         ...product,
-//         price: product.price - (product.price * discountRate)
-//      }
+ function applyDiscount(products, discountRate) {
+     return products.map(product => ({
+        ...product,
+        price: product.price - (product.price * discountRate)
+     }
 
-//      ))
-//  }
+     ))
+ }
 
-//  console.log(applyDiscount(products, .1));
+ console.log(applyDiscount(products, .1));
 
 
 //Task 3
-// let sales = [250, 400, 150, 900, 1200];
+let sales = [250, 400, 150, 900, 1200];
 
-// function calculateTotalRevenue(sales) {
-//     return sales.reduce((total, sale) => total + sale, 0)
-// }
+function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale) => total + sale, 0)
+}
 
-// console.log(`Total Rev. 2024: $${calculateTotalRevenue(sales)}`);
+console.log(`Total Rev. 2024: $${calculateTotalRevenue(sales)}`);
 
 //Task 4
 let employee = {name: "John Doe", salary: 50000, position: "Manager"};
