69027213e6fff1bd827d7ae3591b2a80b627d227
Ashley Le <ashleyle@usf.edu>
2025-02-11 16:37:53 -0500

    full commit, because Dr. Reed is an idiot and forgot

--- cc_example.js
+++ cc_example.js
@@ -11,3 +11,32 @@ let products = [
 // console.log(getProductsByCategory(products, "Electronics"));
 
 // Task 2
+//  function applyDiscount(products, discountRate) {
+//      return products.map(product => ({
+//         ...product,
+//         price: product.price - (product.price * discountRate)
+//      }
+
+//      ))
+//  }
+
+//  console.log(applyDiscount(products, .1));
+
+
+//Task 3
+// let sales = [250, 400, 150, 900, 1200];
+
+// function calculateTotalRevenue(sales) {
+//     return sales.reduce((total, sale) => total + sale, 0)
+// }
+
+// console.log(`Total Rev. 2024: $${calculateTotalRevenue(sales)}`);
+
+//Task 4
+let employee = {name: "John Doe", salary: 50000, position: "Manager"};
+
+function updateSalary(employee, percentageIncrease) {
+    return employee.salary += employee.salary * percentageIncrease;
+}
+
+console.log(updateSalary(employee, .1));
